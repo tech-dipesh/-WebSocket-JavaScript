e9b4f33d84ea0238869517f46ba04ccf94a77634
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2025-04-15 00:04:37 +0530

    start the commiting my learning websocket

---
+++ app.js
@@ -0,0 +1,27 @@
+  import { configDotenv } from "dotenv"
+  import express from "express"
+  import http from "http"
+  import path from "path"
+  import { Server } from "socket.io";
+
+  const app=express();
+  const server=http.createServer(app)
+  const io=new Server(server)
+
+  io.on('connection', (socket)=>{
+    console.log("A new user has connected", socket.id);
+    socket.on('user-message', message=>{
+      console.log("A new user is messaged", message);
+      io.emit("message", message)
+    })
+  })
+
+  const port=9000;
+  app.use(express.static(path.resolve("./public")))
+
+
+  app.get("/", (req, res)=>{
+    return res.sendFile("/public/index.html");
+  })
+
+  server.listen(9000, ()=>console.log("Path is working now"))
